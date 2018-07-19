c4c72321ae81f5c91c70e398d033d34b81133d4f
BIG-shuo <15773136247@163.com>
2018-07-20 00:09:04 +0800

    Add files via upload

---
+++ modify_buff_size.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 19 23:36:54 2018
+
+@author: shuo
+"""
+
+import socket
+
+SEND_BUF_SIZE =  4096
+RECV_BUF_SIZE =  4096
+
+
+
+
+
+def  modify_buff_size():
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    
+    #get  the size of socket's send buffer
+    
+    bufsize = sock.getsockopt(socket.SOL_SOCKET,socket.SO_SNDBUF)
+   
+    print "Buffer size [before]: %d" %bufsize
+    
+    sock.setsockopt(socket.SOL_TCP,socket.TCP_NODELAY,1)
+    sock.setsockopt(
+            socket.SOL_SOCKET,
+            socket.SO_SNDBUF,
+            SEND_BUF_SIZE)
+    
+    
+    sock.setsockopt(
+            socket.SOL_SOCKET,
+            socket.SO_RCVBUF,
+            RECV_BUF_SIZE)
+    
+    bufsize = sock.getsockopt(socket.SOL_SOCKET,socket.SO_SNDBUF)
+    print "Buffer size [After] : %d" %bufsize
+    
+    
+if __name__ == '__main__':
+    modify_buff_size()
+    
+ 
